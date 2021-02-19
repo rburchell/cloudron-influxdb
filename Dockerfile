e40226f64d23fce1fd27aab606549bbed7bf8748
Robin Burchell <robin.burchell@crimson.no>
2021-02-19 23:20:20 +0100

    Fixes to use InfluxDB 1.8.x

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM influxdb:latest as final
+FROM influxdb:1.8.4 as final
 
 FROM cloudron/base:2.0.0@sha256:f9fea80513aa7c92fe2e7bf3978b54c8ac5222f47a9a32a7f8833edf0eb5a4f4
 COPY --from=final /usr/bin/influxd /app/code/influxd
 COPY --from=final /usr/bin/influx /app/code/influx
 
-ADD --chown=cloudron:cloudron start.sh config.json /app/code/
+ADD --chown=cloudron:cloudron start.sh influxd.conf /app/code/
 
 EXPOSE 8086
 RUN mkdir -p /app/data && chown -R cloudron:cloudron /app/data && chmod +x /app/code/start.sh
--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ set -eu
 
 echo "=> sending config"
 
-if [ ! -f "/app/data/config.json" ]; then 
-	cp /app/code/config.json /app/data/config.json
+if [ ! -f "/app/data/influxd.conf" ]; then
+	cp /app/code/influxd.conf /app/data/influxd.conf
 fi
 export INFLUXD_CONFIG_PATH=/app/data
 
@@ -13,6 +13,5 @@ if [ ! -f "/app/data/.configured" ]; then
 	mkdir -p /app/data/influx/data /app/data/influx/meta /app/data/influx/wal
 	touch /app/data/.configured
 fi
-export INFLUXD_CONFIG_PATH=/app/data/config.json
 echo "=> starting influxdb"
-exec "/app/code/influxd"
+exec "/app/code/influxd" -config /app/data/influxd.conf
